5d3e4781c0408b43dfad9eb59de855103b13f6b3
davidhalasz <davidhalasz22@gmail.com>
2022-07-24 15:36:29 +0200

    szelektiv oldal csinositas

--- resources/views/szelektiv-hulladekgyujtes.blade.php
+++ resources/views/szelektiv-hulladekgyujtes.blade.php
@@ -1,120 +1,184 @@
 <x-guest-layout>
-    <div class="container mx-auto">
-        <div class="flex flex-wrap">
-            <div class="w-full mt-32">
-                <div class="pr-0 xl:pr-10">
-                    <p>
-                        A szelektív hulladékgyűjtés az ipari újrahasznosítás egy elengedhetetlen része. Megkönnyíthetjük vele a hulladék válogatásának folyamatát és meggátolhatjuk a hasznosítható hulladék beszennyeződését is. A szelektív gyűjtésnek számos formája terjedt el. Ezek közül a legismertebbek:
+    <div class="mx-auto pt-16 bg-basegreen">
+        <div class="flex flex-wrap items-center container szelektiv-level">
+            <div class="szelektiv-content">
+                <img src="{{ asset('images/level.png') }}" />
+                <div class="w-6/12 pr-0">
+                    <h1 class="font-bold text-3xl sm:text-4xl md:text-[40px] text-white mb-6">
+                        Szelektív hulladékgyűjtés
+                    </h1>
+
+                    <p class="text-white">
+                        A szelektív hulladékgyűjtés az ipari újrahasznosítás egy elengedhetetlen része. Megkönnyíthetjük
+                        vele a hulladék válogatásának folyamatát és meggátolhatjuk a hasznosítható hulladék
+                        beszennyeződését
+                        is. A szelektív gyűjtésnek számos formája terjedt el. Ezek közül a legismertebbek:
                     </p>
-                    <p class="text-2xl font-semibold text-darkblue mt-12 mb-2">
-                        Száraz-nedves gyűjtés
-                    </p>
-                    <p>
-                        Falvakban előforduló módszer, amelynek lényege hogy az újrahasznosítható anyagok (fa,fém,papír,textil) jellemzően mind a száraz kukába kerülnek, amely jelentősen megkönnyíti a válogatást és meggátolja a szennyeződéseket. A nedves hulladékgyűjtőbőbe a szennyezett nedves háztartási hulladékok kerülnek. Amennyiben a háztartásban élők nem komposztálnak, a felhasználható hulladék kiválogatására a nedves tárolókból már nem lesz lehetőség. 
-                    </p>
-                    <p class="text-2xl font-semibold text-darkblue mt-12 mb-2">
-                        Zsákos gyűjtés
-                    </p>
-                    <p>
-                        Szintén főleg falvakban, családi házas övezetekben elterjedt megoldást, ahol a nagy távolságok miatt nem tud ideálisan működni egy gyűjtőszigetes kialakítás. A lakók számára környezetbarát anyagból készült zsákokat biztosítanak, amelyekbe az újrahasznosítható anyagokat vegyesen tárolják. A zsákok szállítása adott időszakonként történik (kéthetente vagy havonta)
-                    </p>
-                    <p class="text-2xl font-semibold text-darkblue mt-12 mb-2">
-                        Hulladékgyűjtő szigetek
-                    </p>
-                    <p>
-                        Településközpontok, tömbházas övezetek közelében kerül elhelyezésre. A papír és műanyag mindenhol, míg az üveg és ruhagyűjtő csak néhány helyen lelhető fel.
-                    </p>
-                    <p class="text-2xl font-semibold text-darkblue mt-12 mb-2">
-                        Hulladékudvar
-                    </p>
-                    <p>
-                        Kevésbé elterjedt és népszerű megoldás, annak ellenére, hogy az egyik legideálisabb hely olyan hulladékok (veszélyes anyagok, gumiabroncsok, akkumulátorok) leadására, amelyeket sem a háztartási hulladékkal, sem a gyűjtőszigeteken nem adhatunk le. A leadható hulladékok listája külön táblázatban került összefoglalásra. 
-                    </p>
-                    <p class="text-2xl font-semibold text-darkblue mt-12 mb-2">
-                        Akciószerű gyűjtőprogramok
-                    </p>
-                    <p>
-                        Civil szervezetek és felelősséget átvállaló cégek időszakosan szervezett hulladékgyűjtése, amelyen főleg olyan hulladéktípusokat vesznek át, amelyeket nem célszerű vagy megengedett a lakossági hulladék közé dobni (elemek, étolaj, elektronika, üveg)
-                    </p>
-                    <p>
-                        Az említett megoldások közül a hulladékszigetek és a hulladékudvarok kerülnek bemutatásra:
-                    </p>
-                    <table class="border-collapse border border-slate-500">
-                        <thead>
-                          <tr>
-                            <th class="border border-slate-600">Hulladékgyűjtő</th>
-                            <th class="border border-slate-600">Mit dobhatok bele</th>
-                            <th class="border border-slate-600">Mit nem dobhatok bele</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr>
-                            <td class="border border-slate-700">Papír</td>
-                            <td class="border border-slate-700">
-                                <ul class="list-disc">
-                                    <li>
-                                        Vegyes papír, hullámkarton-papír: újságok, folyóiratok, füzetek, könyvek, hullámpapír, csomagolópapír, kisebb papírdobozok, hullámkarton-papír
-                                    </li>
-                                    <li>
-                                        Italos kartondobozok (tetrapak): Helyi szabályozástól függően műanyagba dobandó
-                                    </li>
-                                </ul>
-                            </td>
-                            <td class="border border-slate-700">
-                                Zsíros, szennyezett papírok, műanyag borítók, hőpapír, használt egészségügyi papírok (ideértve a zsebkendőt, törlőkendőt és pelenkát is)
-                            </td>
-                          </tr>
-                          <tr>
-                            <td class="border border-slate-700">fehér üveg</td>
-                            <td class="border border-slate-700">
-                                fehér üveg (átlátszó öblösüveg): befőttesüveg, italos üvegpalack, bébiételes üvegek
-                            </td>
-                            <td rowspan="2" class="border border-slate-700">
-                                Síküveg: ablaküveg, tükör, villanykörte, neoncső, hőálló üvegtál, szemüveg lencséje, veszélyes anyagokat tartalmazó üvegek, porcelán, kerámia
-                            </td>
-                          </tr>
-                          <tr>
-                            <td class="border border-slate-700">színes üveg</td>
-                            <td class="border border-slate-700">
-                                színes üveg: borosüveg, pezsgősüveg. sörösüveg
-                            </td>
-                          </tr>
-                          <tr>
-                            <td class="border border-slate-700">Műanyag és
-                                Fém</td>
-                            <td class="border border-slate-700">
-                                PET palackok, műanyagflakonok, műanyagszatyrok, háztartásban előforduló tiszta fóliák
-                            </td>
-                            <td rowspan="2" class="border border-slate-700">
-                                Műanyagjátékok, CD- és DVD-lemez, vegyszert tartalmazó flakonok, ételmaradékkal vagy olajjal szennyezett műanyagok, kávékapszulák, joghurtos, tejfölös, margarinos dobozok, hungarocell
-                            </td>
-                          </tr>
-                          <tr>
-                            <td class="border border-slate-700">(elkülönítés később a válogatóüzemben)</td>
-                            <td class="border border-slate-700">
-                                Fémdobozok, háztartási fémhulladékok. befőttesüveg fémfedele, fémkupakok. 
-                            </td>
-                          </tr>
-                          <tr>
-                            <td class="border border-slate-700">Zöldhulladék (elsősorban kerti hulladék)</td>
-                            <td class="border border-slate-700">
-                                Lehullott lom, lenyírt fű, faágak
-                            </td>
-                            <td class="border border-slate-700">
-                                konyhai hulladékok
-                            </td>
-                          </tr>
-                          <tr>
-                            <td class="border border-slate-700">Bio</td>
-                            <td class="border border-slate-700">
-                                Kerti hulladék: Lehullott lom, lenyírt fű, faágakKonyhai hulladék: zöldség és gyümölcsmaradékok, kávézacc, teafű, használt virágföld
-                            </td>
-                            <td class="border border-slate-700">
-                                húsok, csontok, folyékony nagyon zsíros vagy sós ételmaradékok, porzsák, macskaalom
-                            </td>
-                          </tr>
-                        </tbody>
-                      </table>
+                </div>
+            </div>
+        </div>
+        <div class="container flex flex-wrap pb-16">
+            <div class="w-full mt-6">
+                <div class="grid grid-cols-2 gap-4">
+                    <div class="bg-black rounded-3xl p-8 text-white">
+                        <p class="text-2xl font-semibold mb-2">
+                            Száraz-nedves gyűjtés
+                        </p>
+                        <p>
+                            Falvakban előforduló módszer, amelynek lényege hogy az újrahasznosítható anyagok
+                            (fa,fém,papír,textil) jellemzően mind a száraz kukába kerülnek, amely jelentősen megkönnyíti
+                            a
+                            válogatást és meggátolja a szennyeződéseket. A nedves hulladékgyűjtőbőbe a szennyezett
+                            nedves
+                            háztartási hulladékok kerülnek. Amennyiben a háztartásban élők nem komposztálnak, a
+                            felhasználható hulladék kiválogatására a nedves tárolókból már nem lesz lehetőség.
+                        </p>
+                    </div>
+
+                    <div class="bg-black rounded-3xl p-8 text-white">
+                        <p class="text-2xl font-semibold mb-2">
+                            Zsákos gyűjtés
+                        </p>
+                        <p>
+                            Szintén főleg falvakban, családi házas övezetekben elterjedt megoldást, ahol a nagy
+                            távolságok
+                            miatt nem tud ideálisan működni egy gyűjtőszigetes kialakítás. A lakók számára
+                            környezetbarát
+                            anyagból készült zsákokat biztosítanak, amelyekbe az újrahasznosítható anyagokat vegyesen
+                            tárolják. A zsákok szállítása adott időszakonként történik (kéthetente vagy havonta)
+                        </p>
+                    </div>
+
+                    <div class="bg-black rounded-3xl p-8 text-white">
+                        <p class="text-2xl font-semibold mb-2">
+                            Hulladékgyűjtő szigetek
+                        </p>
+                        <p>
+                            Településközpontok, tömbházas övezetek közelében kerül elhelyezésre. A papír és műanyag
+                            mindenhol, míg az üveg és ruhagyűjtő csak néhány helyen lelhető fel.
+                        </p>
+                    </div>
+
+                    <div class="bg-black rounded-3xl p-8 text-white">
+                        <p class="text-2xl font-semibold mb-2">
+                            Hulladékudvar
+                        </p>
+                        <p>
+                            Kevésbé elterjedt és népszerű megoldás, annak ellenére, hogy az egyik legideálisabb hely
+                            olyan
+                            hulladékok (veszélyes anyagok, gumiabroncsok, akkumulátorok) leadására, amelyeket sem a
+                            háztartási hulladékkal, sem a gyűjtőszigeteken nem adhatunk le. A leadható hulladékok
+                            listája
+                            külön táblázatban került összefoglalásra.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="mx-auto w-8/12 mt-4">
+                    <div class="bg-black rounded-3xl p-8 text-white">
+                        <p class="text-2xl font-semibold mb-2">
+                            Akciószerű gyűjtőprogramok
+                        </p>
+                        <p>
+                            Civil szervezetek és felelősséget átvállaló cégek időszakosan szervezett hulladékgyűjtése,
+                            amelyen főleg olyan hulladéktípusokat vesznek át, amelyeket nem célszerű vagy megengedett a
+                            lakossági hulladék közé dobni (elemek, étolaj, elektronika, üveg)
+                        </p>
+                        <p class="my-2">
+                            Az említett megoldások közül a hulladékszigetek és a hulladékudvarok kerülnek bemutatásra:
+                        </p>
+
+                        <table class="border-collapse border border-slate-700 mt-8">
+                            <thead>
+                                <tr>
+                                    <th class="border border-slate-600">Hulladékgyűjtő</th>
+                                    <th class="border border-slate-600">Mit dobhatok bele</th>
+                                    <th class="border border-slate-600">Mit nem dobhatok bele</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td class="border border-slate-700 p-4">Papír</td>
+                                    <td class="border border-slate-700 p-4">
+                                        <ul class="list-disc">
+                                            <li>
+                                                Vegyes papír, hullámkarton-papír: újságok, folyóiratok, füzetek,
+                                                könyvek,
+                                                hullámpapír, csomagolópapír, kisebb papírdobozok, hullámkarton-papír
+                                            </li>
+                                            <li class="mt-2">
+                                                Italos kartondobozok (tetrapak): Helyi szabályozástól függően műanyagba
+                                                dobandó
+                                            </li>
+                                        </ul>
+                                    </td>
+                                    <td class="border border-slate-700 p-4">
+                                        Zsíros, szennyezett papírok, műanyag borítók, hőpapír, használt egészségügyi
+                                        papírok
+                                        (ideértve a zsebkendőt, törlőkendőt és pelenkát is)
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="border border-slate-700 p-4">fehér üveg</td>
+                                    <td class="border border-slate-700 p-4">
+                                        fehér üveg (átlátszó öblösüveg): befőttesüveg, italos üvegpalack, bébiételes
+                                        üvegek
+                                    </td>
+                                    <td rowspan="2" class="border border-slate-700 p-4">
+                                        Síküveg: ablaküveg, tükör, villanykörte, neoncső, hőálló üvegtál, szemüveg
+                                        lencséje,
+                                        veszélyes anyagokat tartalmazó üvegek, porcelán, kerámia
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="border border-slate-700 p-4">színes üveg</td>
+                                    <td class="border border-slate-700 p-4">
+                                        színes üveg: borosüveg, pezsgősüveg. sörösüveg
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="p-4 no-border-bottom">Műanyag és
+                                        Fém</td>
+                                    <td class="border border-slate-700 p-4">
+                                        PET palackok, műanyagflakonok, műanyagszatyrok, háztartásban előforduló tiszta
+                                        fóliák
+                                    </td>
+                                    <td rowspan="2" class="border border-slate-700 p-4">
+                                        Műanyagjátékok, CD- és DVD-lemez, vegyszert tartalmazó flakonok, ételmaradékkal
+                                        vagy
+                                        olajjal szennyezett műanyagok, kávékapszulák, joghurtos, tejfölös, margarinos
+                                        dobozok, hungarocell
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="p-4 no-border-top">(elkülönítés később a válogatóüzemben)</td>
+                                    <td class="border border-slate-700 p-4">
+                                        Fémdobozok, háztartási fémhulladékok. befőttesüveg fémfedele, fémkupakok.
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="border border-slate-700 p-4">Zöldhulladék (elsősorban kerti hulladék)</td>
+                                    <td class="border border-slate-700 p-4">
+                                        Lehullott lom, lenyírt fű, faágak
+                                    </td>
+                                    <td class="border border-slate-700 p-4">
+                                        konyhai hulladékok
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="border border-slate-700 p-4">Bio</td>
+                                    <td class="border border-slate-700 p-4">
+                                        Kerti hulladék: Lehullott lom, lenyírt fű, faágakKonyhai hulladék: zöldség és
+                                        gyümölcsmaradékok, kávézacc, teafű, használt virágföld
+                                    </td>
+                                    <td class="border border-slate-700 p-4">
+                                        húsok, csontok, folyékony nagyon zsíros vagy sós ételmaradékok, porzsák,
+                                        macskaalom
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
